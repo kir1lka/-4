f9ac8fbf5aa45d508f957541bb56c37ab89a5f23
LAPTOP-864TU38I\Кирилл <mrralio@mail.ru>
2023-04-04 15:46:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class CustomMath
+    {
+        public static int sum(int x, int y)
+        {
+            return x + y;
+        }
+        public static int division(int x, int y)
+        {
+            if (y == 0)
+                throw new InvalidOperationException("divider = 0”");
+            else return x / y;
+        }
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int z = division(1, 0);
+                Console.WriteLine("Test3 failed");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Test3 passed");
+            }
+        }
+        public static bool main(int x, int y)
+        {
+            bool result = true;
+            if (y == 0)
+                result = false;
+            return result;
+        }
+    }
+}
---
+++ тис4/UnitTest1.cs
@@ -0,0 +1,96 @@
+﻿using ConsoleApp2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace тис4
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        int num = 0;
+        [TestMethod]
+        public void Test1()
+        {
+            Console.WriteLine($"Номер теста : {num}");
+            Console.WriteLine($"Дата и время : {DateTime.Now}");
+        }
+
+
+        [TestMethod]
+        [Ignore]
+        public void Test2()
+        {
+            Console.WriteLine("Sum");
+            int x = 0;
+            int y = 0;
+            int expResult = 0;
+            int result = CustomMath.sum(x, y);
+
+            Assert.AreEqual(expResult, result);
+            num++;
+        }
+
+
+
+        [TestMethod]
+        public void Test3()
+        {
+            Console.WriteLine("division");
+            int x = 2;
+            int y = 1;
+            int expResult = 2;
+            try
+            {
+                int result = CustomMath.division(x, y);
+                Assert.AreEqual(expResult, result);
+                if (y == 0)
+                    Console.WriteLine("Деление на ноль не создает исключительной ситуации");
+            }
+            catch (DivideByZeroException e)
+            {
+                if (y != 0)
+                    Console.WriteLine("Генерация исключения при ненулевом элементе");
+            }
+            num++;
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            Console.WriteLine("main");
+
+            int x = 0;
+            int y = 0;
+            bool expResult = false;
+            bool result = CustomMath.main(x, y);
+
+            Assert.IsFalse(result);
+            num++;
+        }
+
+
+        [TestMethod]
+        public void Test5()
+        {
+            Console.WriteLine("main");
+
+            int x = 0;
+            int y = 0;
+            bool expResult = false;
+            bool result = CustomMath.main(x, y);
+
+            Assert.IsFalse(result);
+            num++;
+        }
+        public static bool main(int x, int y)
+        {
+            bool result = true;
+            if (y == 0)
+                result = false;
+            return result;
+        }
+
+    }
+}
+
